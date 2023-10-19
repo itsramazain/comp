7d866ed6c240a2d8e7e27edae235f00b231f1310
itsramazain <104790048+itsramazain@users.noreply.github.com>
2023-10-19 19:33:52 +0300

    Update Branch_Target_Calculator.v

--- comp101/CPU/Branch_Target_Calculator.v
+++ comp101/CPU/Branch_Target_Calculator.v
@@ -1,11 +1,16 @@
-module Branch_Target_Calculator(
-    input [15:0] immediate,           // Immediate field from the instruction - bits 0-15
-    input [31:0] program_counter,     // Program counter of the current instruction
-    output reg [31:0] BT              // Output for the branch target
-);
-
-    // The branch target is calculated by adding the sign-extended immediate value to the program counter.
-    always @(*)
-        BT = {immediate[15], immediate} + program_counter;
-
+module Branch_Target_calculator(
+		input [7:0]immediate,//immedate filed from the instruction
+		input [7:0]program_counter, //nest instruction
+		output reg[7:0]BT
+		
+		);
+		/*becase the nextinstrction is saved in a word manner we dont need to shift it by 2*/
+		always@(*)
+		
+			BT= immediate[7:0]+ program_counter;
+		
 endmodule
+
+		
+		
+		

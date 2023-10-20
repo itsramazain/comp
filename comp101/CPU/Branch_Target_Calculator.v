b83d83023d246e95d3e3ddb9e5adfd423114f16a
Ad31 Sh <68187289+ad31sh@users.noreply.github.com>
2023-10-20 19:07:09 +0300

    Update Branch_Target_Calculator.v

--- comp101/CPU/Branch_Target_Calculator.v
+++ comp101/CPU/Branch_Target_Calculator.v
@@ -1,16 +1,12 @@
-module Branch_Target_calculator(
-		input [7:0]immediate,//immedate filed from the instruction
-		input [7:0]program_counter, //nest instruction
-		output reg[7:0]BT
-		
-		);
-		/*becase the nextinstrction is saved in a word manner we dont need to shift it by 2*/
-		always@(*)
-		
-			BT= immediate[7:0]+ program_counter;
-		
-endmodule
+module Branch_Target_Calculator(
+    input [7:0] immediate,          // Immediate field from the instruction
+    input [7:0] program_counter,    // Next instruction's program counter
+    output reg [7:0] BT             // Branch target output
+);
 
-		
-		
-		
+    // The next instruction is saved in a word manner, so no need to shift it by 2
+    always @(*) begin
+        BT = immediate[7:0] + program_counter;  // Calculate the branch target
+    end
+
+endmodule

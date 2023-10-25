db2c88804c3a2f6600923351648f27cfdb6187ae
itsramazain <104790048+itsramazain@users.noreply.github.com>
2023-10-25 18:33:27 +0300

    Update Mux2x1.v

--- comp101/CPU/Mux2x1.v
+++ comp101/CPU/Mux2x1.v
@@ -1,11 +1,13 @@
-module mux2x1 (
+module Mux2x1 (
 	
     input [n-1:0] i0,  // Input data for '0' selector
     input [n-1:0] i1,  // Input data for '1' selector
     input sel,        // Selection control (0 for 'i0', 1 for 'i1')
     output reg [n-1:0] out  // Output data based on selection
 );
+
 parameter n=32;
+
     always @(*) begin
         if (sel) begin
             out = i1;  // When 'sel' is 1, output 'i1'
